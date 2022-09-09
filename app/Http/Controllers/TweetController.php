695588853a25244893933d27701c9c4dc20b4374
nuno8825 <ruinoeru1031@gmail.com>
2022-09-09 20:19:20 +0900

    commit 9/09

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -83,7 +83,8 @@ class TweetController extends Controller
      */
     public function edit($id)
     {
-        //
+        $tweet = Tweet::find($id);
+        return view('tweet.edit', compact('tweet'));
     }
 
     /**
@@ -95,7 +96,21 @@ class TweetController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+            //バリデーション
+        $validator = Validator::make($request->all(), [
+            'tweet' => 'required | max:191',
+            'description' => 'required',
+        ]);
+        //バリデーション:エラー
+        if ($validator->fails()) {
+            return redirect()
+            ->route('tweet.edit', $id)
+            ->withInput()
+            ->withErrors($validator);
+        }
+        //データ更新処理
+        $result = Tweet::find($id)->update($request->all());
+        return redirect()->route('tweet.index');
     }
 
     /**
@@ -106,6 +121,7 @@ class TweetController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $result = Tweet::find($id)->delete();
+        return redirect()->route('tweet.index');
     }
 }
